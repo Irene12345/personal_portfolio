2f33ab8f429f39bce63dacae73b26638a97add8c
Irene12345 <15231820+Irene12345@users.noreply.github.com>
2024-06-12 18:01:16 +0200

    Click on cat

---
+++ script.js
@@ -0,0 +1,10 @@
+let meaowing = document.getElementById('meaow');
+let catpic = document.getElementById('cat');
+
+function showMeaow() {
+    meaowing.style.visibility = 'visible';
+}
+
+catpic.addEventListener('click', showMeaow);
+
+
